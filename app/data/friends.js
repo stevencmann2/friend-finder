e277d6d2f6d359020cce55783b29003aacc1d014
Steven Mann <stevencmann2@gmail.com>
2019-12-18 14:09:06 -0600

    added data parsing to server.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,104 +1,98 @@
 // Pre-made friend objects used in app
-const friends = [
-{
-    "name": "Rachel",
-    "photo": "https://pmctvline2.files.wordpress.com/2019/06/friends-rachel.jpg?w=620",
-    "scores": [
-        1,
-        2,
-        5,
-        4,
-        3,
-        2,
-        2,
-        1,
-        2,
-        5
-    ]
-}, 
-{
-    "name": "Ross",
-    "photo": "https://i2.wp.com/whatwouldbaledo.com/wp-content/uploads/2016/05/ross.jpg?fit=625%2C415&ssl=1",
-    "scores": [
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1,
-        1
-    ]
-}, 
-{
-    "name": "Joey",
-    "photo": "https://www.cheatsheet.com/wp-content/uploads/2019/10/matt-le-blanc-1024x745.jpg",
-    "scores": [
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5
-    ]
-}, 
-{
-    "name": "Phoebe",
-    "photo": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/03/03/16/phoebe-friends.jpg?w968h681",
-    "scores": [
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3,
-        3  
-    ]
-}, 
-{
-    "name": "Chandler",
-    "photo": "https://i.pinimg.com/originals/5b/8e/59/5b8e592514209b33db6259858d8248a0.jpg",
-    "scores": [
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2,
-        2  
-    ]
-}, 
-{
-    "name": "Monica",
-    "photo": "https://usercontent1.hubstatic.com/14597034.jpg",
-    "scores": [
-       4,
-       4,
-       4,
-       4,
-       4,
-       4,
-       4,
-       4,
-       4,
-       4  
-    ]
-}, 
-
-
-
-
-
+const friends = [{
+        "name": "Rachel",
+        "photo": "https://pmctvline2.files.wordpress.com/2019/06/friends-rachel.jpg?w=620",
+        "scores": [
+            1,
+            2,
+            5,
+            4,
+            3,
+            2,
+            2,
+            1,
+            2,
+            5
+        ]
+    },
+    {
+        "name": "Ross",
+        "photo": "https://i2.wp.com/whatwouldbaledo.com/wp-content/uploads/2016/05/ross.jpg?fit=625%2C415&ssl=1",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name": "Joey",
+        "photo": "https://www.cheatsheet.com/wp-content/uploads/2019/10/matt-le-blanc-1024x745.jpg",
+        "scores": [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Phoebe",
+        "photo": "https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/03/03/16/phoebe-friends.jpg?w968h681",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "Chandler",
+        "photo": "https://i.pinimg.com/originals/5b/8e/59/5b8e592514209b33db6259858d8248a0.jpg",
+        "scores": [
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Monica",
+        "photo": "https://usercontent1.hubstatic.com/14597034.jpg",
+        "scores": [
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4,
+            4
+        ]
+    },
 ]
--- server.js
+++ server.js
@@ -11,3 +11,6 @@ app.listen(PORT, () =>
     console.log(`The server is listening on port ${PORT}!`));
 
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
