1b44dfcdc5a486865d3a62d9ce9d56cb8713e95b
Steven Mann <stevencmann2@gmail.com>
2019-12-18 15:11:06 -0600

    handled routing error to directory path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,15 +6,15 @@ module.exports =  function (app){
 
 // GET ROUTE FOR APP 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 // POST ROUTE FOR SURVEY ANSWERS
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
+    res.sendFile(path.join(__dirname, "./public/survey.html"));
   });
 
   app.use( function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
 }
--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 const path = require('path');
 
-app.get('/', (req, res) =>
-    res.send('Hello Worldz!'));
+// app.get('/', (req, res) =>
+//     res.send('Hello Worldz!'));
 
 app.listen(PORT, () =>
     console.log(`The server is listening on port ${PORT}!`));
@@ -14,3 +14,6 @@ app.listen(PORT, () =>
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Require route paths in server using htmlRoutes.js
+require('./app/routing/htmlRoutes.js')(app);
