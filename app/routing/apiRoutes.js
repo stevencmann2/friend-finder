32332262107ef756f8670c0eedbcd5ebc1cc1fdf
Steven Mann <stevencmann2@gmail.com>
2019-12-20 14:53:33 -0600

    need to debug form validation, name when empty is issue

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,14 +12,11 @@ module.exports = function (app) {
 
   app.post("/api/friends", function (req, res) {
     friendsData.push(req.body);
-    res.json(true)
+    res.json(true);
 });
 
 }
 
 
-
-
-// GET ROUTES FOR APP 
 
 
