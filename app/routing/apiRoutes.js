508a8f214b5ad1bf8f157630fcbe7d0327fa5dea
Steven Mann <stevencmann2@gmail.com>
2019-12-20 18:24:32 -0600

    created sum function to determine result of absolute value array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,7 +14,7 @@ module.exports = function (app) {
       friendsData.push(req.body);
       res.json(true);
 
-      
+
       /////////inital code for response posted comparison//////
       let postedData = req.body;
 
@@ -22,27 +22,35 @@ module.exports = function (app) {
       if (postedData) {
         //new array of posted scores truned from strings into numbers
         let numberArray = []; 
-        let sc
+        /////// sum function for in comparison operator
+        function sumArray (arr){
+          let sum = 0;
+          for (let s=0; s < arr.length; s++){
+            sum = sum + arr[s];
+          }
+        return sum    
+        }
+
+        
         //Creates new Array of numbers from posted user data
         for (let i = 0; i < postedData.scores.length; i++) {
           let userNum = parseInt(postedData.scores[i])
           numberArray.push(userNum)
         };
 
-        // console.log(numberArray)
-        // console.log(friendsData.length -2 )
-
         // walking backwards down the array to exclude the most recent posted 
         for (let i=friendsData.length-2; i >=0 ; i--){
         //  console.log(friendsData[i].name + friendsData[i].scores + "   " + numberArray) //shows name/score of compared friend
+        let avArray = [];
             for(n=0;n<friendsData[i].scores.length; n++ ){
-              let indexComparisonAV = Math.abs( numberArray[n] - friendsData[i].scores[n]);
-              console.log(friendsData[i].name + ` position of${n} in the array `  + 
-                          ' with a value of '+ friendsData[i].scores[n]+
-                          " compared to " + numberArray[n] + " = " + indexComparisonAV)
-
+              let indexComparisonAV =+ Math.abs( numberArray[n] - friendsData[i].scores[n]);
+              // console.log(friendsData[i].name + ` position of${n} in the array `  + 
+              //             ' with a value of '+ friendsData[i].scores[n]+
+              //             " compared to " + numberArray[n] + " = " + indexComparisonAV)
+              avArray.push(indexComparisonAV)   // creates new array of absolute values by index
             } 
-            
+            console.log(avArray);
+            console.log(sumArray(avArray));  // returns name of compared object plus the AV
         }
        
 
@@ -56,3 +64,5 @@ module.exports = function (app) {
 
   )
 };
+
+
